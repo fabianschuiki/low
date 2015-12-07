aa8b2dd7ab20a1044285f9cf2fc0fd6b21525cff
Fabian Schuiki <fabian@schuiki.ch>
2015-12-07 12:25:25 +0100

    Implement separate test script
    Change the way tests are executed by adding a script that will iterate
    over each *.low file in the test/ directory and perform tests on it. The
    test is configured from within the *.low file, by placing the markers
    "+compile", "-compile", "+execute", or "-execute" somewhere in the file.
    Tests are compiled and optionally run, with exit codes interpreted
    according to the +/- prefix. This makes the folder structure in test/
    redundant, but it can be kept for clarity if desired.

---
+++ test/run_tests.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# Copyright (c) 2015 Fabian Schuiki
+# Runs a set of tests.
+
+set -e
+
+# assume "lowc" as the default low compiler
+if [ -z "$LOWC" ]; then
+	LOWC=lowc
+fi
+
+# assume "lli" as the default LLVM interpreter
+if [ -z "$LLI" ]; then
+	LLI=lli
+fi
+
+echo "0" > .test_passed
+echo "0" > .test_failed
+
+hr() { printf "%*s\n" "${COLUMNS:-$(tput cols)}" "" | tr " " -; }
+log_skip() { printf "\r[skip]  %s\n" "$1"; }
+log_fail() { printf "\r[`tput setaf 1`fail`tput sgr0`]  %s\n" "$1"; expr `cat .test_failed` + 1 > .test_failed; }
+log_pass() { printf "\r[`tput setaf 2`pass`tput sgr0`]  %s\n" "$1"; expr `cat .test_passed` + 1 > .test_passed; }
+
+# iterate over all tests in the test directory
+DIR=$(dirname $0)
+find $DIR -name "*.low" -print0 | while read -d $'\0' TEST; do
+	TESTLL=${TEST%.low}.ll
+	TEST_NAME=${TEST#$DIR/}
+	printf "[....]  %s" "$TEST_NAME"
+
+	# determine what to expect from this test
+	COMP_PASS=$(grep -ic -m 1 "\+compile" "$TEST" || true)
+	COMP_FAIL=$(grep -ic -m 1 "\-compile" "$TEST" || true)
+	EXEC_PASS=$(grep -ic -m 1 "\+execute" "$TEST" || true)
+	EXEC_FAIL=$(grep -ic -m 1 "\-execute" "$TEST" || true)
+	if [ $EXEC_PASS == 1 ] || [ $EXEC_FAIL == 1 ]; then
+		if [ $COMP_FAIL == 1 ]; then
+			log_fail "$TEST_NAME"
+			printf "        test wants to execute but also to fail compilation\n"
+			continue
+		fi
+		COMP_PASS=1
+	fi
+
+	# make sure any form of compilation was configured
+	if [ $COMP_PASS == 0 ] && [ $COMP_FAIL == 0 ]; then
+		log_skip "$TEST_NAME"
+		continue
+	fi
+
+	# compile the program
+	if "$LOWC" "$TEST" 1>.out 2>&1; then
+		if [ $COMP_FAIL = 1 ]; then
+			log_fail "$TEST_NAME"
+			printf "        compilation succeeded, but should have failed\n"
+			continue
+		fi
+	else
+		if [ $COMP_PASS = 1 ]; then
+			log_fail "$TEST_NAME"
+			printf "        compilation failed\n"
+			hr
+			cat .out
+			hr
+			continue;
+		fi
+	fi
+
+	# execute the program if configured that way
+	if [ $EXEC_PASS == 1 ] || [ $EXEC_FAIL == 1 ]; then
+		if "$LLI" "$TESTLL" 1>.out 2>&1; then
+			if [ $EXEC_FAIL = 1 ]; then
+				log_fail "$TEST_NAME"
+				printf "      execution succeeded, but should have failed\n"
+				continue
+			fi
+		else
+			if [ $EXEC_PASS = 1 ]; then
+				log_fail "$TEST_NAME"
+				printf "      execution failed\n"
+				hr
+				cat .out
+				hr
+				continue;
+			fi
+		fi
+	fi
+
+	log_pass "$TEST_NAME"
+done
+
+NUM_PASSED=`cat .test_passed`
+NUM_FAILED=`cat .test_failed`
+
+rm .test_passed
+rm .test_failed
+if [ -e .out ]; then
+	rm .out
+fi
+
+if [ $NUM_FAILED = 0 ]; then
+	printf "\n  all %d tests passed\n\n" $NUM_PASSED
+	exit 0
+else
+	NUM_TOTAL=$(expr $NUM_FAILED + $NUM_PASSED)
+	printf "\n  %d/%d tests failed\n\n" $NUM_FAILED $NUM_TOTAL
+	exit 1
+fi
