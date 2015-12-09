ad7848fdc9824825fe11acdacc5e4d6535d08a7b
Thomas Richner <mail@trichner.ch>
2015-12-09 21:22:26 +0100

    attempting to fix travis build

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -ev
+
+# g++4.8
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.8"; fi
+# clang 3.6
+if [ "$CXX" == "clang++" ]; then export CXX="clang++-3.6"; fi
+
 cmake -DCMAKE_BUILD_TYPE=debug .
 make
 make tests
